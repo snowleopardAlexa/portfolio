d4db188e695bb8b2595db865f52a94d28b53df03
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-17 18:52:38 -0400

    change html

---
+++ the-lion-king/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.menu-toggle').document('click', function() {
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+    });
+
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+

95f5d8db19158c027eb155b4a620d17d41f31a2f
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-20 13:29:52 -0400

    add modal html

--- the-lion-king/js/script.js
+++ the-lion-king/js/script.js
@@ -1,14 +1,3 @@
-$(document).ready(function() {
-    $('.menu-toggle').document('click', function() {
-        $('.nav').toggleClass('showing');
-        $('.nav ul').toggleClass('showing');
-    });
 
-    $('.post-wrapper').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-});
+
 

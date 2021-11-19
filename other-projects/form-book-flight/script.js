104e3ef05f886b527825f3de738d302dea719c39
snowleopardAlexa <alexaslomski@gmail.com>
2021-11-18 19:28:50 -0500

    add functions

--- other-projects/form-book-flight/script.js
+++ other-projects/form-book-flight/script.js
@@ -1,3 +1,34 @@
 // DECREMENT AND INCREMENT BUTTONS 
 
+// ADULTS
+
+function increment() {
+    document.getElementById('numOfAdults').stepUp();
+}
+
+function decrement() {
+    document.getElementById('numOfAdults').stepDown();
+}
+
+// KIDS
+
+function increment() {
+    document.getElementById('numOfKids').stepUp();
+}
+
+function decrement() {
+    document.getElementById('numOfKids').stepDown();
+}
+
+// PETS
+
+function increment() {
+    document.getElementById('numOfPets').stepUp();
+}
+
+function decrement() {
+    document.getElementById('numOfPets').stepDown();
+}
+
+
 
